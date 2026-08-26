ca66266325fdd34a85761735ebeaf6f01756738e
gokhangokcen1 <gokhangokcenn@gmail.com>
2026-08-26 12:14:30 +0300

    variables done

--- 01-C/02-variables.c
+++ 01-C/02-variables.c
@@ -3,14 +3,29 @@
 
 int main(){
     char myLetter = 'C';
-    long int myNumber = 5147483647;
+    int myNumber = 57;
     float myNumber2 = 2.56;
     double myNumber3 = 213.568;
     
     printf("%c\n", myLetter);
-    printf("%ld\n", myNumber);
+    printf("%d\n", myNumber);
     printf("%f\n", myNumber2);
     printf("%lf\n", myNumber3);
+    printf("%s\n", "where have you been?");
+   
+    int number, number2, extraction, sum, multiplication, division;
+    number = 25;
+    number2 = 5;
+    sum = number + number2;
+    extraction = number - number2;
+    multiplication = number * number2;
+    division = number / number2;
+
+    printf("Toplama: %d\n", sum);
+    printf("Çıkarma: %d\n", extraction);
+    printf("Çarpma: %d\n", multiplication);
+    printf("Bölme: %d\n", division);
+
 
     return 0;
 
